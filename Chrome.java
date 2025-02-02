4cbba93c87637a0b06be3bbf9641119264dc8221
KV-Kannan369 <145201507+KV-Kannan369@users.noreply.github.com>
2025-02-02 07:01:14 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1.homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		/* version of type float with a value of 91.0f
+		2. developer of type String with a value of "Google"
+		3. isBeta of type boolean with a value of false
+		4. releaseYear of type int with a value of 2008
+		5. shortcutKey of type char with a value of 'C'*/
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaeYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.print("Chrome version: ==> "+version+"\n"+
+		"Developer: ==>"+developer+"\n"+"IsBeta version: ==>"+isBeta+"\n"+"Releae year:==>"+releaeYear+"\n"+
+				"shortcutKey ==>"+shortcutKey);
+
+	}
+
+}
