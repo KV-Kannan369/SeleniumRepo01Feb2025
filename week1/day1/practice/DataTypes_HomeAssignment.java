4d9034abe87703eda9d4cf6b5aa32128e3738043
KV-Kannan369 <145201507+KV-Kannan369@users.noreply.github.com>
2025-02-02 07:24:43 +0530

    Add files via upload

---
+++ week1/day1/practice/DataTypes_HomeAssignment.java
@@ -0,0 +1,24 @@
+package week1.day1.practice;
+
+public class DataTypes_HomeAssignment {
+	public static void main(String[] args) {
+		byte age = 30;
+		short roomnumber = 103;
+		int amountDeposited = 50000;
+		long amountDeposted2 = 30000000l;
+		boolean doYouLikeJava	= true;
+		char gender = 'M';
+		float rateOfInt = 11.1234567890f;
+		double rateOfInt2 = 11.1234567890;
+		
+		System.out.println("1.(byte)Age -->" +age);
+		System.out.println("2.(short)roomnumber -->" +roomnumber);
+		System.out.println("3.(int)amountDeposited -->" +amountDeposited);
+		System.out.println("4.(long)amountDeposted2 -->" +amountDeposted2);
+		System.out.println("5.(boolean)doYouLikeJava -->" +doYouLikeJava);
+		System.out.println("6.(char)gender -->" +gender);
+		System.out.println("7.(float)rateOfInt -->"+rateOfInt);
+		System.out.println("8.(double)rateOfInt2 -->" +rateOfInt2);
+	}
+
+}
